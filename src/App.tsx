958077df52a2dda660a94e6a64feb08ac152c9eb
bijink <kbijin528@gmail.com>
2021-12-28 17:43:15 +0530

    Add Item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,54 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+// Components
+import { Badge, Drawer, Grid, LinearProgress } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+import Item from "./Item/Item";
+// Styled
+import { Wrapper } from "./App.styles";
+
+// Types
+export type CartItemType = {
+   id: number;
+   category: string;
+   description: string;
+   image: string;
+   price: number;
+   title: string;
+   amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+   await (await fetch('https://fakestoreapi.com/products')).json();
+
+
 const App = () => {
+   // fetch('https://fakestoreapi.com/products/1')
+   //    .then(res => res.json())
+   //    .then(json => console.log(json));
+
+   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+   console.log(data);
+
+   const getTotalItems = () => null;
+   const handleAddToCart = (clickedItem: CartItemType) => null;
+   const handleRemoveFromCart = () => null;
+
+   if (isLoading) return <LinearProgress />;
+   if (error) return <div>Something went wrong...</div>;
+
    return (
-      <div className="App">
-         Hello
-      </div>
+      <Wrapper>
+         <Grid container spacing={3}>
+            {
+               data?.map((item => (
+                  <Grid item key={item.id} xs={12} sm={4}>
+                     <Item item={item} handleAddToCart={handleAddToCart} />
+                  </Grid>
+               )))
+            }
+         </Grid>
+      </Wrapper>
    );
 };
 
